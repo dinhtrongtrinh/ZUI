3f9038aad15e7c4f4fb64ca810fd22bea055f4df
Trong Trinh Dinh <dinhtrongtrinh04@gmail.com>
2026-03-24 10:35:20 +0100

    hotovy prvni ukol opt

--- Prvni ukol/tempwages.py
+++ Prvni ukol/tempwages.py
@@ -1,46 +1,28 @@
 import numpy as np
 
 def fit_temps(t: np.ndarray, T: np.ndarray, omega: float):
-    col_ones = np.ones(len(t))
-    col_sin = np.sin(omega * t)
-    col_cos = np.cos(omega * t)
-
-    A_mat = np.stack((col_ones,t,col_sin,col_cos), axis = 1)
-    AT_mat = np.stack((col_ones,t,col_sin,col_cos), axis = 0)
-
-    #(AT*A)^-1
-    ATA_inverse = np.linalg.inv(np.dot(AT_mat, A_mat))
-
-    #AT*T
-    ATb_mat = np.dot(AT_mat,T)
-
-    x = np.dot(ATA_inverse,ATb_mat)
+    # Vytvoření matice plánu (design matrix) přímo v NumPy
+    # Sloupce: [1, t, sin(omega*t), cos(omega*t)]
+    A = np.column_stack((
+        np.ones_like(t), 
+        t, 
+        np.sin(omega * t), 
+        np.cos(omega * t)
+    ))
+    
+    # Elegantní řešení soustavy A*x = T pomocí metody nejmenších čtverců
+    # lstsq vrací (řešení, rezidua, hodnost, singulární hodnoty)
+    x, _, _, _ = np.linalg.lstsq(A, T, rcond=None)
     return x
 
 def fit_wages(t: np.ndarray, M: np.ndarray):
-    one_array = np.ones(len(t))
-
-    #A , AT
-    A_matrix = np.stack((one_array,t), axis = 1) 
-    AT_matrix = np.stack((one_array,t), axis = 0) 
-
-    #AT*A
-    ATA_matrix = np.dot(AT_matrix, A_matrix)
-
-    #(AT*A)^-1
-    ATA_inverse = np.linalg.inv(ATA_matrix)
-
-    ATb_mat = np.dot(AT_matrix,M)
-
-    #(AT*A)^-1 * AT *b
-    x = np.dot(ATA_inverse,ATb_mat)
+    # Matice plánu pro lineární trend: [1, t]
+    A = np.column_stack((np.ones_like(t), t))
+    
+    # Opět použijeme lstsq pro stabilitu
+    x, _, _, _ = np.linalg.lstsq(A, M, rcond=None)
     return x
     
-def quarter2_2009(x:np.ndarray):
-    M = x[0] + x[1] * 2009.25
-    return M
-
-
-
-
-
+def quarter2_2009(x: np.ndarray):
+    # 2009.25 odpovídá druhému kvartálu (duben-červen)
+    return x[0] + x[1] * 2009.25
